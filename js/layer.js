fd091c23f9812890bd9b7aa14e3cf94acee1abe0
Nguyen Hoang Tung <nguyenhoangtung@sadboi.local>
2024-10-31 22:37:12 +0700

    second commit

--- js/layer.js
+++ js/layer.js
@@ -4,10 +4,12 @@ var items = document.getElementsByClassName("nav-item");
 for (var i = 0; i < items.length; i++) {
   items[i].addEventListener('mouseover', function () {
     overlay.style.opacity = 0.8; 
+    overlay.style.zIndex = 999;
   });
 
   items[i].addEventListener('mouseleave', function () {
     overlay.style.opacity = 0; 
+    overlay.style.zIndex = -1;
   });
 }
 
